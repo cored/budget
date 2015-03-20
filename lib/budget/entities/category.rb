55835be1e9193f42b45fe702b51ec6e336c4dd1e
cored <george.rafael@gmail.com>
2015-03-19 22:45:53 -0400

    Add proper Category relationship with Expense

--- lib/budget/entities/category.rb
+++ lib/budget/entities/category.rb
@@ -2,4 +2,12 @@ class Category
   include Lotus::Entity
 
   attributes :name
+
+  def expense_subtotal 
+    expenses.inject(0) { |subtotal, expense| subtotal += expense.amount }
+  end
+
+  def expenses
+    ExpenseRepository.find_by_category_id(id)
+  end
 end
